80ecb94a358ee46dbf668855bb8bf8277f809624
Aiden <aiden.chandran@elev.ga.lbs.se>
2025-11-03 12:10:28 +0100

    z

--- Assets/Scrpits/Main menu.cs
+++ Assets/Scrpits/Main menu.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Mainmenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void PlayGame()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void Quitgame()
+    {
+        Application.Quit();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
