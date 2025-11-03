46c349c429ea05c6f273fddf9fcdce0a761e6f4b
hnscrft <hnnsmncrft@gmail.com>
2025-11-03 14:27:22 +0100

    Added Zombie enemy

---
+++ Assets/Scrpits/Enemies/BasicZombie.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(BoxCollider2D))]
+public class BasicZombie : Enemy
+{
+    [SerializeField] float climbForce;
+    bool needToClimb;
+    private void Update()
+    {
+        if (target != null)
+        {
+            ChasePlayer();
+            isAttacking = true;
+        }
+        else
+        {
+            Patrol();
+        }
+    }
+
+    void Patrol()
+    {
+        rb.AddForce(Vector2.up * patrolSpeed);
+    }
+
+    void ChasePlayer()
+    {
+        if (target.position.x > transform.position.x)
+        {
+            rb.AddForce(Vector2.right * chaseSpeed);
+        }
+
+        if (target.position.x < transform.position.x)
+        {
+            rb.AddForce(Vector2.left * chaseSpeed);
+        }
+
+        if (needToClimb)
+        {
+            rb.AddForce(Vector2.up * climbForce);
+        }
+
+        print($"{gameObject.name} is chasing player");
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.transform.position.y + collision.transform.localScale.y / 2 > transform.position.y && collision.gameObject.layer == 3) //if we are not standing on the objects roof
+        {
+            needToClimb = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        needToClimb = false;
+    }
+}
