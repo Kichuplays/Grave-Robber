c038ffaa33285fe638ef90b5538e4563c88c135b
hnscrft <hnnsmncrft@gmail.com>
2025-11-04 10:18:02 +0100

    Enemy update
    changed zombie layer, readded enemy layer and began creating ghost

--- Assets/Scrpits/Enemies/EnemyTest.cs
+++ Assets/Scrpits/Enemies/EnemyTest.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
+[RequireComponent(typeof(Seeker))]
 
 public class EnemyTest : Enemy
 {
-
+    
 }
